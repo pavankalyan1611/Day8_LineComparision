72dcd8708851a94368aabfbd8e252a4c2be1a821
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-07-17 10:55:56 +0530

    [pavan] Checking Lengths Equal or Not

--- Day8LineComparisionOops/Day8LineComparisionOops/Line.cs
+++ Day8LineComparisionOops/Day8LineComparisionOops/Line.cs
@@ -12,7 +12,7 @@ namespace Day8LineComparisionOops
         {
             Console.WriteLine("welcome to Line Comparision Problem :");
         }
-        public double Length1()
+        public double Length()
         {
             Console.WriteLine("Enter the Value of X1 for Cal Length1");
             int x1 = Convert.ToInt32(Console.ReadLine());
@@ -22,8 +22,8 @@ namespace Day8LineComparisionOops
             int y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the Value of Y2 for Cal Length1");
             int y2 = Convert.ToInt32(Console.ReadLine());
+
             double result = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("Length of the Line1:  " + result);
             return result;
         }
 
--- Day8LineComparisionOops/Day8LineComparisionOops/Program.cs
+++ Day8LineComparisionOops/Day8LineComparisionOops/Program.cs
@@ -1,4 +1,14 @@
 ﻿using Day8LineComparisionOops;
 
+
 Line line = new Line();
-Console.WriteLine(line.Length1());
+double result1 = line.Length();
+Console.WriteLine("Length Of Line1: {0}", result1);
+double result2 = line.Length();
+Console.WriteLine("Length Of Line2: {0}", result2);
+
+if (result1 == result2)
+    Console.WriteLine("Length1 and length2  Are Equal");
+else
+    Console.WriteLine("Length1 and length2  Are not Equal");
+
